14ca84b7eebdfb4bf3194fb49e47a08efd48d6b2
sowmyadsl <sowmya.dsl@gmail.com>
2017-04-07 12:02:16 -0700

    Add new order button and add logic for the user to add another pizza

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,63 @@ Pizza.prototype.price = function () {
 function Order(name,number){
   this.name = name;
   this.number = number;
+  this.pizzas= [];
 }
 
-
-
+Order.prototype.totalprice = function(){
+  var totalprice  = this.pizzas;
+}
 
 
 //UserInterface Logic
 $(document).ready(function(){
-
-
-
-
-
-
-
-
+  $("#add-button").click(function(){
+     $("#add-order").append('<div class="new-order">' +
+                               '<h4>Choose up to 3 toppings:</h4>' +
+                               '<div class="form-group">' +
+                                 '<label for="toppings"><em>Topping 1:</em></label>' +
+                                 '<select class="form-control topping1">' +
+                                   '<option selected="true" value="0">Please select one:</option>' +
+                                   '<option value="1">Cheese</option>' +
+                                   '<option value="1.5">Onions</option>' +
+                                   '<option value="1.75">Olives</option>' +
+                                   '<option value="1.25">Mushrooms</option>' +
+                                   '<option value="2">Artichokes</option>' +
+                                 '</select>' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="toppings"><em>Topping 2:</em></label>' +
+                                 '<select class="form-control topping2">' +
+                                   '<option selected="true" value="0">Please select one:</option>' +
+                                   '<option value="1">Cheese</option>' +
+                                   '<option value="1.5">Onions</option>' +
+                                   '<option value="1.75">Tomatoes</option>' +
+                                   '<option value="1.25">Mushrooms</option>' +
+                                   '<option value="2">Artichokes</option>' +
+                                 '</select>' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="toppings"><em>Topping 3:</em></label>' +
+                                 '<select class="form-control topping3">' +
+                                   '<option selected="true" value="0">Please select one:</option>' +
+                                   '<option value="1">Cheese</option>' +
+                                   '<option value="1.5">Onions</option>' +
+                                   '<option value="1.75">Olives</option>' +
+                                   '<option value="1.25">Red peppers</option>' +
+                                   '<option value="2">Artichokes</option>' +
+                                 '</select>' +
+                               '</div>' +
+                               '<h4>Choose a size:</h4>' +
+                               '<div class="form-group">' +
+                                 '<select class="form-control size">' +
+                                   '<option selected="true" value="0">Please select one:</option>' +
+                                   '<option value="5">Small</option>' +
+                                   '<option value="10">Medium</option>' +
+                                   '<option value="15">Large</option>' +
+                                 '</select>' +
+                               '</div>' +
+                             '</div>');
+   });
 
 
   $("#orderform").submit(function(event){
@@ -38,16 +79,25 @@ $(document).ready(function(){
     var inputNumber = $("#number").val();
     var newOrder = new Order(inputName,inputNumber);
 
-    var topping1 = parseFloat($(".topping1").val());
-    var topping2 = parseFloat($(".topping2").val());
-    var topping3 = parseFloat($(".topping3").val());
-    var size = parseFloat($(".size").val());
+    $(".new-order").each(function() {
+    var topping1 = parseFloat($(this).find(".topping1").val());
+    var topping2 = parseFloat($(this).find(".topping2").val());
+    var topping3 = parseFloat($(this).find(".topping3").val());
+    var size = parseFloat($(this).find(".size").val());
+    var newPizza = new Pizza(topping1, topping2, topping3, size);
+    console.log(newOrder.pizzas.push(newPizza));
+    });
 
-    var pizza = new Pizza(topping1,topping2,topping3,size);
+    // var topping1 = parseFloat($(".topping1").val());
+    // var topping2 = parseFloat($(".topping2").val());
+    // var topping3 = parseFloat($(".topping3").val());
+    // var size = parseFloat($(".size").val());
+    //
+    // var pizza = new Pizza(topping1,topping2,topping3,size);
 
-    
+
     $("#insert-name").text(newOrder.name);
-    $("#price").text(pizza.price());
+    $("#price").text(newPizza.totalprice());
     $("#output, #delivery-option").show();
     $("#order").show();
 
@@ -69,7 +119,18 @@ $(document).ready(function(){
     $("#insert-address").text(address);
     $("#delivery").show();
     $("#address").hide();
-
-
   });
+
+  $("button#no-deliver").click(function(){
+    $("#delivery-option").hide();
+     $("#pick-up, #new-order-button").show();
+  });
+
+  $("#new-order").click(function(event){
+     $("#order").reset();
+     $("#address").reset();
+     $("#output, #pick-up, #delivery, #new-order-button").hide();
+     $("#order").show();
+   });
+
   });
