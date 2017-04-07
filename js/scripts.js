0a9a3bfbb430c3de16a1f13f97e3bca3a15360c3
sowmyadsl <sowmya.dsl@gmail.com>
2017-04-07 10:45:05 -0700

    Add a banner image and jquery logic for delivery and pick up options

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 //Business Logic
+function Pizza(topping1,topping2,topping3,size){
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
+  this.size = size;
+}
+
+Pizza.prototype.price = function () {
+  return this.topping1 + this.topping2 + this.topping3 + this.size;
+};
+
+
 function Order(name,number){
   this.name = name;
   this.number = number;
@@ -8,22 +20,56 @@ function Order(name,number){
 
 
 
-
-
-
-
-
-
-
-
 //UserInterface Logic
 $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
   $("#orderform").submit(function(event){
     event.preventDefault();
     var inputName = $("#name").val();
     var inputNumber = $("#number").val();
     var newOrder = new Order(inputName,inputNumber);
 
-    console.log(newOrder);
+    var topping1 = parseFloat($(".topping1").val());
+    var topping2 = parseFloat($(".topping2").val());
+    var topping3 = parseFloat($(".topping3").val());
+    var size = parseFloat($(".size").val());
+
+    var pizza = new Pizza(topping1,topping2,topping3,size);
+
+    
+    $("#insert-name").text(newOrder.name);
+    $("#price").text(pizza.price());
+    $("#output, #delivery-option").show();
+    $("#order").show();
+
   });
-});
+
+  $("#yes-deliver").click(function(){
+    $("#output, #delivery-option").hide();
+    $("#address").show();
+
+  });
+
+  $("#address").submit(function(event){
+    event.preventDefault();
+    var inputStreet = $("#street").val();
+    var inputCity = $("#city").val();
+    var inputState = $("#state").val();
+    var inputZipcode = $("#zipcode").val();
+    var address = (inputStreet + "," + inputCity + "," + inputState + "," + inputZipcode).toUpperCase();
+    $("#insert-address").text(address);
+    $("#delivery").show();
+    $("#address").hide();
+
+
+  });
+  });
